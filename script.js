4b763961b933d5d391a485d0ff861ef5bd34b2d0
z20axa <alejandrospan@hotmail.com>
2022-11-05 19:38:21 -0400

    Adds JS code for secon fetch call

--- script.js
+++ script.js
@@ -12,16 +12,50 @@ function getApi() {
     var cityName = citynameEL.value;
     console.log('City Name', cityName);
 
-    var requestURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + openweatherAPIKey;
+    var onedayrequestURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + openweatherAPIKey;
 
-    fetch(requestUrl)
+    console.log('Request URL', onedayrequestURL);
+
+    fetch(onedayrequestURL)
         .then(function (response) {
         return response.json();
         })
         .then(function (data) {
+            console.log('Data', data);
+            console.log('Data', data.main.temp);
+            // need a moment command to convert number to actual date
+            console.log('Lat', data.coord.lat);
+            console.log('Lon', data.coord.lon);
+
+            // make sure to pay attention to when object and array to get actual value 
+            var iconcode = data.weather[0].icon;
+            var iconurimgEl = `http://openweathermap.org/img/w/${iconcode}.png`;
+
+
+            var lon = data.coord.lon;
+            var lat = data.coord.lat;
+
+            var fivedaysrequestURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${openweatherAPIKey}`;
+
+            fetch(fivedaysrequestURL)
+            .then(function (response) {
+            return response.json();
+            })
+            .then(function (data) {
+                console.log('Data', data);
+    
+    
+    
+          
+        });
+
       
     });
+
+
 };
+
+
 
 // click event listener when the city name is submitted and function call to getApi
 submitbtnEl.addEventListener('click', getApi);
