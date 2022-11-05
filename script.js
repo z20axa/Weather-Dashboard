b9ec47e7aaf28abfecdbdc75deedb1f0baa6ef10
z20axa <alejandrospan@hotmail.com>
2022-11-05 17:23:02 -0400

    Adds HTML and JS code for function declaration

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 console.log('hello');
 
+citynameEL = document.querySelector('#cityName');
+submitbtnEl = document.querySelector('#submitBtn');
+
+console.log('City Name', cityName);
+
+
+var openweatherAPIKey = "86428bd2b8af57a99daa14d368265a5f";
+
+var cityName;
+
+function getApi() {
+
+    var requestURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + openweatherAPIKey;
+
+    fetch(requestUrl)
+        .then(function (response) {
+        return response.json();
+        })
+        .then(function (data) {
+      
+    });
+};
+
+submitbtnEl.addEventListener('click', getApi);
