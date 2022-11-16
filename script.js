bc5fb53d5a526e1b152bedc8d67a23a356053713
z20axa <alejandrospan@hotmail.com>
2022-11-15 23:15:20 -0500

    Adds HTML and JS code to display weather info for city entered

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // DOM elements variable declarations
-citynameEL = document.querySelector('#cityName');
+inputCitynameEl = document.querySelector('#inputCityName');
 submitbtnEl = document.querySelector('#submitBtn');
+spanCityNameEl = document.querySelector('#spanCityName');
+imgTempIconEl = document.querySelector('#imgTempIcon');
+
 
 // variable declaration for my open weather API key
 var openweatherAPIKey = "86428bd2b8af57a99daa14d368265a5f";
@@ -9,27 +12,41 @@ var openweatherAPIKey = "86428bd2b8af57a99daa14d368265a5f";
  * function declaration 
  */
 function getApi() {
-    var cityName = citynameEL.value;
+    // variable declaration for the entered city name
+    var cityName = inputCitynameEl.value;
     console.log('City Name', cityName);
+
+    // modify the text/attributes for the city entered to be displayed
+    spanCityNameEl.textContent = cityName;
+
 
     var onedayrequestURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${openweatherAPIKey}`;
 
-    console.log('Request URL', onedayrequestURL);
+    // console.log('Request URL', onedayrequestURL);
 
     fetch(onedayrequestURL)
         .then(function (response) {
         return response.json();
         })
         .then(function (data) {
-            console.log('Data', data);
-            console.log('Data', data.main.temp);
+            // console.log('Data', data);
+            // console.log('Temp Kelvin', data.main.temp);
+            
             // need a moment command to convert number to actual date
-            console.log('Lat', data.coord.lat);
-            console.log('Lon', data.coord.lon);
+            // console.log('Lat', data.coord.lat);
+            // console.log('Lon', data.coord.lon);
 
             // make sure to pay attention to when object and array to get actual value 
-            var iconcode = data.weather[0].icon;
-            var iconurimgEl = `http://openweathermap.org/img/w/${iconcode}.png`;
+            var currentDayIconCode = data.weather[0].icon;
+            // console.log("Current Day Icon Code", currentDayIconCode)
+            var currentDayIconPage = `http://openweathermap.org/img/w/${currentDayIconCode}.png`;
+            // console.log("Icon", currentDayIconPage);
+            imgTempIconEl.setAttribute("src", currentDayIconPage);
+            imgTempIconEl.setAttribute("alt", "Weather Icon");
+
+
+
+          
 
 
             var lon = data.coord.lon;
@@ -38,24 +55,14 @@ function getApi() {
             var fivedaysrequestURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${openweatherAPIKey}`;
 
             fetch(fivedaysrequestURL)
-            .then(function (response) {
-            return response.json();
-            })
-            .then(function (data) {
-                console.log('Data', data);
-    
-    
-    
-          
-        });
-
-      
+                .then(function (response) {
+                return response.json();
+                })
+                .then(function (data) {
+                    // console.log('Second fetch Data', data);
+        }); 
     });
-
-
 };
-
-
 
 // click event listener when the city name is submitted and function call to getApi
 submitbtnEl.addEventListener('click', getApi);
