fd6f51ef6bad90375d2154fc7990c7f1460006cf
z20axa <alejandrospan@hotmail.com>
2022-11-21 16:46:07 -0500

    Adds variables declarations

--- script.js
+++ script.js
@@ -1,40 +1,53 @@
 // DOM elements variable declarations
-inputCitynameEl = document.querySelector('#inputCityName');
-submitbtnEl = document.querySelector('#submitBtn');
+inputCityNameEl = document.querySelector('#inputCityName');
+submitBtnEl = document.querySelector('#submitBtn');
 spanCityNameEl = document.querySelector('#spanCityName');
 imgTempIconEl = document.querySelector('#imgTempIcon');
 
 
-// variable declaration for my open weather API key
-var openweatherAPIKey = "86428bd2b8af57a99daa14d368265a5f";
+// variable declarations 
+var openWeatherAPIKey = "86428bd2b8af57a99daa14d368265a5f"; // for open weather API key
+var cityNameSubmitted = ""; // for user submitted city name
+var spanCityName = ""; // for city name to be displayed
+var currenDayTemp = ""; // for current day temp
+
+
+
+
+
 
 /**
  * function declaration for API calls to get data and modiy attributes of the DOM element variables
  */
 function getApi() {
-    // variable declaration for the entered city name
-    var cityName = inputCitynameEl.value;
+
+    cityNameSubmitted = inputCityNameEl.value;
     // console.log('City Name', cityName);
 
-    // modify the text/attributes for the city entered to be displayed
-    spanCityNameEl.textContent = cityName;
-
     // varibale declaration for URL to get current day weather data
-    var onedayrequestURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${openweatherAPIKey}`;
+    var onedayRequestURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityNameSubmitted}&appid=${openWeatherAPIKey}`;
     // console.log('Request URL', onedayrequestURL);
 
     // fetch request to get data from API for current day weather condition 
-    fetch(onedayrequestURL)
+    fetch(onedayRequestURL)
         .then(function (response) {
         return response.json();
         })
         .then(function (data) {
             // console.log('Data', data);
+            // console.log('City Name', data.name);
+            // console.log('Temp in K', data.main.temp);
+            // console.log('Wind', data.wind.speed);
+            // console.log('Humidity', data.main.humidity);
+
+            // variables assigments from API data 
+            spanCityName = data.name;
 
 
-            
 
-            // console.log('Temp Kelvin', data.main.temp);
+            // modify the text/attributes for the city entered to be displayed
+            spanCityNameEl.textContent = spanCityName;
+
             // fiveDayForeDay1.textContent = "Temp: " + Math.floor((data.list[0].main.temp - 273.15) * 1.8 + 32) + "°F";
             
             // need a moment command to convert number to actual date
@@ -52,18 +65,18 @@ function getApi() {
             var lon = data.coord.lon;
             var lat = data.coord.lat;
 
-            var fivedaysrequestURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${openweatherAPIKey}`;
+            var fivedaysRequestURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${openWeatherAPIKey}`;
 
             // fetch request to get data from API for the next 5 days using lon and lat data from previous fecth call 
-            fetch(fivedaysrequestURL)
+            fetch(fivedaysRequestURL)
                 .then(function (response) {
                 return response.json();
                 })
                 .then(function (data) {
-                    // console.log('Second fetch Data', data);
+                // console.log('Second fetch Data', data);
         }); 
     });
 };
 
 // click event listener when the city name is submitted and function call to getApi
-submitbtnEl.addEventListener('click', getApi);
+submitBtnEl.addEventListener('click', getApi);
